c037f7b17b27a78bea04ee7d0ab4e42cfcfc129f
fender5692 <fender5692@gmail.com>
2017-03-26 18:06:09 -0400

    Update TUI.cpp

--- TUI.cpp
+++ TUI.cpp
@@ -11,129 +11,150 @@
 #include <fstream>
 #include <string>
 #include <menu.h>
-#include <string.h>
-#include <panel.h>
-using namespace std;
-
-int main()
-{	WINDOW *my_win;
-	PANEL  *my_panel;
-	int lines = 30, cols = 120, y = 2, x = 4, i;
-
-	initscr();
-	cbreak();
-	noecho();
-
-	/* Create window for the panel */
-	my_win = newwin(lines, cols, y, x);
-
-
-	/* 
-	 * Create border around the window
-	 */
-		box(my_win, 0, 0);
-
-	/* Attach a panel to the window */ 
-	my_panel = new_panel(my_win); 	
-
-
-	/* Update the panel */
-	update_panels();
-
-	/* Show it on the screen */
-	doupdate();
-	
-	getch();
-	endwin();
-}
-
-//Example of window with border (not using panels)//
-#include <cstdlib>
-#include <ncurses.h>
-#include <iostream>
-#include <fstream>
 #include <string>
-#include <menu.h>
-#include <string.h>
 #include <panel.h>
+#include <cerrno>
+#include <unistd.h>
+#include <fcntl.h>
 using namespace std;
 
-WINDOW *create_newwin(int height, int width, int starty, int startx);
-void destroy_win(WINDOW *local_win);
+//void print_in_middle(WINDOW *win, int starty, int startx, int width, const char *string, chtype color);
+/**
+* Main method for program
+*
+*/
+int main(const int argc, const char * argv []){
 
-int main(int argc, char *argv[])
-{	WINDOW *my_win;
-	int startx, starty, width, height;
-	int ch;
+  initscr();//open window
+  
+  WINDOW * myPad;
+  int row, col;
+  getmaxyx(stdscr, row, col);
+  myPad = newpad(row + 1000, col);
+  keypad(myPad, TRUE);
+  const int BUFF_SIZE = 1024; // size of data buffer
+  char buffer [BUFF_SIZE];    // data buffer 
+  int readVal = 0;
 
-	initscr();			/* Start curses mode 		*/
-	cbreak();			/* Line buffering disabled, Pass on
-					 * everty thing to me 		*/
-	keypad(stdscr, TRUE);		/* I need that nifty F1 	*/
+  
 
-	height = 30;
-	width = 120;
-	starty = (LINES - height) / 2;	/* Calculating for a center placement */
-	startx = (COLS - width) / 2;	/* of the window		*/
-	printw("Press F1 to exit");
-	refresh();
-	my_win = create_newwin(height, width, starty, startx);
+  int pos = 0;
+  //If file is given to upon initialization of program//
+  if(argc == 2){
+    string fn = "";
+    fn = argv[1];               // Set the filename
+    
+    ifstream myfile(fn.c_str());
+    string line;
+    //Check for valid file//
+    if (myfile.is_open())
+    {
+      //Loops through text in file and prints to screen//
+      while ( getline (myfile,line) )
+      {
+        wprintw(myPad, line.c_str());
+        wprintw(myPad, "\n");
+      }
+      myfile.close();
+      prefresh(myPad, pos, 0, 0, 0, row -1, col);
+    }
+  
+    else printw("Unable to open file");
+  }//if
+  int ch;
+    //cursor position//
+  int x = 0;
+  int y = 0;
+  
+  while(1){
+    ch = wgetch(myPad);
+      switch(ch){
+      case KEY_UP:
+        if(y != 0){
+          y--;
+          wmove(myPad, y, x);
+          prefresh(myPad, pos, 0 , 0, 0, y, x);
+        }//if
+      break;
+      
+      case KEY_DOWN:
+        y++;
+        wmove(myPad, y, x);
+        prefresh(myPad, pos, 0, 0, 0, y, x);
+      break;
+      
+      case KEY_RIGHT:
+        if(x != col){
+          x++;
+        }//if
+        else{
+          x = 0;
+          y++;
+        }//else
+        wmove(myPad, y, x);
+        prefresh(myPad, pos, 0, 0, 0, y, x);
+        
+        break;
+      
+      case KEY_LEFT:
+        if(x != 0){
+          x--;
+        }//if
+        else{
+          x = col;
+          y--;
+        }//else
+        wmove(myPad, y, x);
+        prefresh(myPad, pos, 0, 0, 0, y, x);
+        break;
+        
+      case '\b' :
+        wdelch(myPad);
+        if(x != 0){
+          x--;
+        }//if
+        else{
+          x = col;
+          y--;
+        }//else
+        prefresh(myPad, pos, 0, 0, 0, y, x);
+        
+        
+      default:
+        waddch(myPad, ch);
+        x++;
+        prefresh(myPad, pos, 0, 0, 0, row -1, col);
+        break;
 
-	while((ch = getch()) != KEY_F(1))
-	{	switch(ch)
-		{	case KEY_LEFT:
-				destroy_win(my_win);
-				my_win = create_newwin(height, width, starty,--startx);
-				break;
-			case KEY_RIGHT:
-				destroy_win(my_win);
-				my_win = create_newwin(height, width, starty,++startx);
-				break;
-			case KEY_UP:
-				destroy_win(my_win);
-				my_win = create_newwin(height, width, --starty,startx);
-				break;
-			case KEY_DOWN:
-				destroy_win(my_win);
-				my_win = create_newwin(height, width, ++starty,startx);
-				break;	
-		}
-	}
-		
-	endwin();			/* End curses mode		  */
-	return 0;
-}
+      }//switch
+    }//while
+    
 
-WINDOW *create_newwin(int height, int width, int starty, int startx)
-{	WINDOW *local_win;
-
-	local_win = newwin(height, width, starty, startx);
-	box(local_win, 0 , 0);		/* 0, 0 gives default characters 
-					 * for the vertical and horizontal
-					 * lines			*/
-	wrefresh(local_win);		/* Show that box 		*/
-
-	return local_win;
-}
-
-void destroy_win(WINDOW *local_win)
-{	
-	/* box(local_win, ' ', ' '); : This won't produce the desired
-	 * result of erasing the window. It will leave it's four corners 
-	 * and so an ugly remnant of window. 
-	 */
-	wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
-	/* The parameters taken are 
-	 * 1. win: the window on which to operate
-	 * 2. ls: character to be used for the left side of the window 
-	 * 3. rs: character to be used for the right side of the window 
-	 * 4. ts: character to be used for the top side of the window 
-	 * 5. bs: character to be used for the bottom side of the window 
-	 * 6. tl: character to be used for the top left corner of the window 
-	 * 7. tr: character to be used for the top right corner of the window 
-	 * 8. bl: character to be used for the bottom left corner of the window 
-	 * 9. br: character to be used for the bottom right corner of the window
-	 */
-	wrefresh(local_win);
-	delwin(local_win);
-}
+  
+  
+  
+/*
+  //Moves page up and down if file is to big.//
+  while(1){
+    
+    switch(ch){
+      case KEY_UP:
+        pos--;
+        prefresh(myPad, pos, 0, 0, 0, row -1, col);
+        ch = wgetch(myPad);
+      break;
+      
+      case KEY_DOWN:
+      pos++;
+      prefresh(myPad, pos, 0, 0, 0, row -1, col);
+      ch = wgetch(myPad);
+      break;
+      
+    }//switch 
+  }//while 
+  */
+  
+  //Close window and exit program//
+  endwin();             
+  return EXIT_SUCCESS;
+}//main
